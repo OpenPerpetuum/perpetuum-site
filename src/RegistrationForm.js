32d110d0704ab06a859cec181e20b4d2a30a255e
Bart Brinkman <bart@instantmagazine.com>
2018-04-14 02:33:06 +0200

    update success url for register form

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -5,7 +5,7 @@ import Input from './Input';
 export default class RegistrationForm extends Component {
   render () {
     return (
-      <Form action="/onboarding/register" actionLabel="Register" success="/register/success">
+      <Form action="/onboarding/register" actionLabel="Register" success="/register/sent">
         <Input type="text" name="email" label="Email" icon="envelope" validation help="This will also be your username. It will never be shown publicly." />
         <Input type="password" name="password" label="Password" icon="key" validation />
         <Input type="password" name="password-repeat" label="Confirm password" icon="redo" validation />
